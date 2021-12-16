3cebbc4e7c2e721e341f39897c4eff3b78d47106
HamCore <thehamcoree@gmail.com>
2021-12-16 12:21:52 +0300

    refactoring main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import random
+from typing import Iterator
 
 from faker import Faker
 
@@ -12,100 +13,107 @@ faker = Faker()
 
 
 def main():
-    res = create_dict(1)
-    res_array = []
-    for i in range(99):  # fixme list comprehension
-        data = (next(res))
-        res_array.append(data)
+    """Creating .json file with data"""
+    result_dict = create_dict(pk=1)
+
+    list_of_objects = []
+
+    for i in range(100):
+        list_of_objects.append(next(result_dict))
+
     with open(FILE_NAME_WRITING, 'w', encoding='utf-8') as f:
-        json.dump(res_array, f, ensure_ascii=False, indent=4)
+        json.dump(list_of_objects, f, ensure_ascii=False, indent=4)
 
 
-def create_dict(pk) -> dict:  # todo Iterator[dict]
+def create_dict(pk) -> Iterator[dict]:
+    """
+    :param pk: (int) autoincrement integer which increase while new object generation
+    :return: Iterator[dict]
+    """
     while True:
         result_dict = {
             'model': add_model_to_dict(),
             'pk': pk,
             'fields': {
-                'title': add_fields_title_to_dict(),  #
-                'year': add_fields_year_to_dict(),  #
+                'title': add_fields_title_to_dict(),
+                'year': add_fields_year_to_dict(),
                 'pages': add_fields_pages_to_dict(),
                 'isbn13': add_fields_isbn13_to_dict(),
-                'rating': add_fields_rating_to_dict(max_value=5),
-                'price': add_fields_price_to_dict(min_price=10, max_price=10000),
+                'rating': add_fields_rating_to_dict(),
+                'price': add_fields_price_to_dict(),
                 'author': [
-                    author1(authors=['Alex', 'Jhon', 'Luci']), author2(authors=['Alex', 'Jhon', 'Luci'])
+                    author(authors=['Alex', 'Jhon', 'Luci']), author(authors=['Michael', 'Aria', 'Lamar'])
                 ],
 
             }
         }
+
         yield result_dict
+
         pk += 1
 
 
 def add_model_to_dict() -> str:
     """
-    :return:
+    :return: (str) string value from file conf.py
     """
     return MODEL
 
 
-def add_fields_title_to_dict() -> str:
+def add_fields_title_to_dict() -> list[str]:
+    """
+    :return: (list[str]) list of values from FILE_NAME_READING
+    """
     with open(FILE_NAME_READING, encoding='utf-8') as f:
         my_fields = f.readlines()
         my_fields = [line.rstrip() for line in my_fields]
-
         return faker.random_element(elements=my_fields)
 
 
 def add_fields_year_to_dict() -> int:
     """
-    :return:
+    :return: (int) random years
     """
     return int(faker.year())
 
 
 def add_fields_pages_to_dict(min_value=10, max_value=9999) -> int:
     """
-    :param min_value:
-    :param max_value:
-    :return:
+    :param min_value: (int) minimum value integer of pages
+    :param max_value: (int) maximum value integer of pages
+    :return: (int) random value of integer between minimum value and maximum value
     """
     return int(faker.random_int(min=min_value, max=max_value))
 
 
 def add_fields_isbn13_to_dict() -> str:
     """
-    :return:
+    :return: (str) isbn for book
     """
     return faker.isbn13()
 
 
-def add_fields_rating_to_dict(max_value) -> float:
+def add_fields_rating_to_dict(max_value=5) -> float:
     """
-    :param max_value:
-    :return:
+    :param max_value: (int) maximum floating point number of range(0, 5)
+    :return: (float) random floating point number
     """
     return random.random() * max_value
 
 
-def add_fields_price_to_dict(min_price, max_price) -> int:
+def add_fields_price_to_dict(min_price=10, max_price=10000) -> int:
     """
-    :param min_price:
-    :param max_price:
-    :return:
+    :param min_price: (int) minimum price in range of price
+    :param max_price: (int) maximum price in range of price
+    :return: (int) random integer from this range
     """
     return random.randint(min_price, max_price)
 
 
-def author1(authors) -> list[str]:
-    return faker.random_element(elements=authors)
-
-
-def author2(authors) -> list:  # todo убрать
+def author(authors) -> list[str]:
     """
-    :param authors:
-    :return:
+    :param authors: list which contains names
+    :return: random value from list
     """
     return faker.random_element(elements=authors)
 
