f04f845db5fbf27e5aaba0613ac59be913b16fd1
Semen <thehamcoree@gmail.com>
2021-12-24 15:06:03 +0300

    1. Refactoring function on dict comprehension 2. Refactoring function *author*

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from faker import Faker
 
 from conf import MODEL
 
+from typing import List
+
 FILE_NAME_READING = 'books.txt'
 FILE_NAME_WRITING = 'result.json'
 
@@ -17,9 +19,9 @@ def main():
     result_dict = create_dict(pk=1)
 
     list_of_objects = []
-
     for i in range(100):
-        list_of_objects.append(next(result_dict))
+        dict_of_objects = {key: value for key, value in next(result_dict).items()}
+        list_of_objects.append(dict_of_objects)
 
     with open(FILE_NAME_WRITING, 'w', encoding='utf-8') as f:
         json.dump(list_of_objects, f, ensure_ascii=False, indent=4)
@@ -41,10 +43,7 @@ def create_dict(pk) -> Iterator[dict]:
                 'isbn13': add_fields_isbn13_to_dict(),
                 'rating': add_fields_rating_to_dict(),
                 'price': add_fields_price_to_dict(),
-                'author': [
-                    author(authors=['Alex', 'Jhon', 'Luci']), author(authors=['Michael', 'Aria', 'Lamar'])
-                ],
-
+                'author': author(authors=['Michael', 'Aria', 'Lamar', 'Alex', 'Jon', 'Luci'])
             }
         }
 
@@ -60,7 +59,7 @@ def add_model_to_dict() -> str:
     return MODEL
 
 
-def add_fields_title_to_dict() -> list[str]:
+def add_fields_title_to_dict() -> List[str]:
     """
     :return: (list[str]) list of values from FILE_NAME_READING
     """
@@ -77,7 +76,7 @@ def add_fields_year_to_dict() -> int:
     return int(faker.year())
 
 
-def add_fields_pages_to_dict(min_value=10, max_value=9999) -> int:
+def add_fields_pages_to_dict(min_value: int = 10, max_value: int = 9999) -> int:
     """
     :param min_value: (int) minimum value integer of pages
     :param max_value: (int) maximum value integer of pages
@@ -93,7 +92,7 @@ def add_fields_isbn13_to_dict() -> str:
     return faker.isbn13()
 
 
-def add_fields_rating_to_dict(max_value=5) -> float:
+def add_fields_rating_to_dict(max_value: int = 5) -> float:
     """
     :param max_value: (int) maximum floating point number of range(0, 5)
     :return: (float) random floating point number
@@ -101,7 +100,7 @@ def add_fields_rating_to_dict(max_value=5) -> float:
     return random.random() * max_value
 
 
-def add_fields_price_to_dict(min_price=10, max_price=10000) -> int:
+def add_fields_price_to_dict(min_price: int = 10, max_price: int = 10000) -> int:
     """
     :param min_price: (int) minimum price in range of price
     :param max_price: (int) maximum price in range of price
@@ -110,12 +109,12 @@ def add_fields_price_to_dict(min_price=10, max_price=10000) -> int:
     return random.randint(min_price, max_price)
 
 
-def author(authors) -> list[str]:
+def author(authors) -> List[str]:
     """
     :param authors: list which contains names
     :return: random value from list
     """
-    return faker.random_element(elements=authors)
+    return faker.random_elements(elements=authors, length=random.randrange(1, 4), unique=True)
 
 
 if __name__ == '__main__':
